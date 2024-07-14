161d796734ac4e67def71cc0c5d2e50454682201
irmansyah38 <irmansya146@gmail.com>
2024-07-14 10:53:38 +0700

    fix: delete serial in inialization

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -6,12 +6,8 @@ void inializationWifi()
 {
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
   // put your setup code here, to run once:
-  Serial.begin(115200);
 
-  // reset settings - wipe credentials for testing
-  // wm.resetSettings();
-
-  wm.setConfigPortalBlocking(false);
+    wm.setConfigPortalBlocking(false);
   wm.setConfigPortalTimeout(60);
   // automatically connect using saved credentials if they exist
   // If connection fails it starts an access point with the specified name
